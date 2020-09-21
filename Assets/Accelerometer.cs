d7aedea28ea4a6c787f6807599a8e4cdd76aa3c9
Bjørn Winther <bwinth18@student.aau.dk>
2020-09-21 11:21:02 +0200

    First Commit

---
+++ Assets/Accelerometer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Accelerometer : MonoBehaviour // add lowpass filter to make less jittery.
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetAccelerometerValue();
+    }
+
+    Vector3 GetAccelerometerValue()
+    {
+        Vector3 acc = Vector3.zero;
+        float period = 0.0f;
+
+        foreach (AccelerationEvent evnt in Input.accelerationEvents)
+        {
+            acc += evnt.acceleration * evnt.deltaTime;
+            period += evnt.deltaTime;
+        }
+        if (period > 0)
+        {
+            acc *= 1.0f / period;
+        }
+        return acc;
+    }
+}
---
+++ Assets/UIHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class UIHandler : MonoBehaviour
+{
+    public GameObject img;
+    public void buttonEvent(){
+        img.SetActive(false);
+        Debug.Log("hello world");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
